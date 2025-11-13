1caf956c9527caaee70b94ab461173b1ee5da812
Gaboemi-Duoc <173064947+Gaboemi-Duoc@users.noreply.github.com>
2025-11-13 10:26:31 -0300

    REAL Initial Commit

---
+++ src/main/java/alumno/duoc/golden_cat_api/GoldenCatApiApplication.java
@@ -0,0 +1,13 @@
+package alumno.duoc.golden_cat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenCatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenCatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alumno/duoc/golden_cat_api/GoldenCatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package alumno.duoc.golden_cat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenCatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
